45ea72bb9c9ff0792699a450930ee9a25c0fb40f
KJH <picsmash7@gmail.com>
2022-12-12 20:30:55 +0900

    lotto fix

--- 02_lotto/script_lotto.js
+++ 02_lotto/script_lotto.js
@@ -4,10 +4,20 @@ const drawButton = document.querySelector('#draw');
 const resetButton = document.querySelector('#reset');
 const colors = ['orange', 'skyblue', 'red', 'purple', 'green'];
 const lottoNumbers = [];
+let count = 0;
 
 // paintNumber 함수
-function paintNumber() {
-  numbersDiv.innerHTML = lottoNumbers.join(' ');
+function paintNumber(lottoNumbers) {
+  let display = () => {
+  console.log(count);
+  if (count > 5) {
+    let clear = clearInterval(timeId);
+  }  
+  numbersDiv.innerHTML += lottoNumbers[count] + ' ';
+  count += 1;
+  }
+  let timeId = setInterval(display, 800);
+  // let stop = ;
 }
 
 // 추첨 버튼 클릭 이벤트 핸들링
@@ -19,14 +29,14 @@ drawButton.addEventListener('click', function(){
     } else {
       lottoNumbers.push(number);
       // console.log(lottoNumbers);
-      paintNumber();
     }
   }
+  paintNumber(lottoNumbers);
 })
 
 // 다시 버튼 클릭 이벤트 핸들링
 resetButton.addEventListener('click', function(){
-  console.log('resetButton'); 
+  lottoNumbers.splice(0);
   numbersDiv.innerHTML = "";
 })
 
