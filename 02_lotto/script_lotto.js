2e80a0d0d863796391d04018021f904be9dda45a
KJH <picsmash7@gmail.com>
2022-12-12 18:03:44 +0900

    lotto

--- 02_lotto/script_lotto.js
+++ 02_lotto/script_lotto.js
@@ -1,24 +1,32 @@
 // 요소 선택 및 상수 선언
-const numbersDiv = document.getElementsByClassName('numbers');
-numbersDiv.textContent = "kjh";
-
-console.log(numbersDiv.textContent, 'asdfdf');
-const colors = ['orange', 'skyblue', 'red', 'purple', 'green']
+const numbersDiv = document.querySelector('.numbers');
+const drawButton = document.querySelector('#draw');
+const resetButton = document.querySelector('#reset');
+const colors = ['orange', 'skyblue', 'red', 'purple', 'green'];
+const lottoNumbers = [];
 
 // paintNumber 함수
-function paintNumber(number){
+function paintNumber() {
+  numbersDiv.innerHTML = lottoNumbers.join(' ');
 }
 
 // 추첨 버튼 클릭 이벤트 핸들링
 drawButton.addEventListener('click', function(){
+  while (lottoNumbers.length < 6) {
+    let number = Math.floor(Math.random() * 45 + 1);
+    if (lottoNumbers.includes(number)) {
+      // console.log('sameNum = ', number);
+    } else {
+      lottoNumbers.push(number);
+      // console.log(lottoNumbers);
+      paintNumber();
+    }
+  }
 })
 
 // 다시 버튼 클릭 이벤트 핸들링
 resetButton.addEventListener('click', function(){
+  console.log('resetButton'); 
+  numbersDiv.innerHTML = "";
 })
-
-// pop 함수를 사용하여 배열의 마지막 요소를 삭제
-// shift 함수를 사용하여 배열의 첫 번째 요소를 삭제
-// splice() 함수를 사용하여 배열 요소를 삭제
-// filter() 함수를 사용하여 조건에 일치하는 새 배열을 생성
 
