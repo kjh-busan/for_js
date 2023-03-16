0a8e7be7bd6542797e482a194d64f3a91648530e
kjh-busan <picsmash7@gmail.com>
2023-03-16 23:36:12 +0900

    feat: destruct obj

--- 05_js_before_react/js/destructuring.js
+++ 05_js_before_react/js/destructuring.js
@@ -3,6 +3,24 @@ const numbers = [1, 2, 3];
 // [num1, num2] = numbers;
 
 // 1 3
-[num1, ,num3] = numbers;
+// [num1, ,num3] = numbers;
 
-console.log(num1, num2);
+// console.log(num1, num2);
+
+// const number = 1;
+// const num2 = number;
+
+const person = {
+  name: 'kjh'
+}
+const secondPerson = person;
+person.name = 'KIM';
+// {name: 'KIM'}
+console.log(secondPerson);
+
+const sencondPerson2 = {
+  ...person
+}
+person.name = 'lorem';
+// {name: 'KIM'}
+console.log(sencondPerson2);
