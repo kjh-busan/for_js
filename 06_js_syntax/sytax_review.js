1dce4a28bac00c4ed2e0ed2b9bbef57d7b8c33b9
kjh-busan <picsmash7@gmail.com>
2023-07-26 21:14:28 +0900

    masking2

--- 06_js_syntax/sytax_review.js
+++ 06_js_syntax/sytax_review.js
@@ -1,13 +1,13 @@
-const Today = "20210101";
-Today.replace(/(\d{4})(\d{2})(\d{2})/g, "$1-$2-$3");
-console.log("Today: ", Today);
+// const Today = "20210101";
+// Today.replace(/(\d{4})(\d{2})(\d{2})/g, "$1-$2-$3");
+// console.log("Today: ", Today);
 
-const Today2 = "20210101";
-Today2.substr(0, 4) + "年" + Today2.substr(4, 2) + "月";
-console.log("Today2: ", Today2);
-
+// const Today2 = "20210101";
+// Today2.substr(0, 4) + "年" + Today2.substr(4, 2) + "月";
+// console.log("Today2: ", Today2);
 
 function maskingEmail(email) {
+  console.log("maskingEmail() start");
   const mask = "*".repeat(email.split("@")[0].length - 1);
   console.log("mask: ", mask);
   return email[0] + mask + email.slice(mask.length + 1, email.length);
