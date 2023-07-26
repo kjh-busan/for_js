0467afa941527e5f1effc2de74c65fee2a192233
kjh-busan <picsmash7@gmail.com>
2023-07-26 21:20:15 +0900

    masking

--- 06_js_syntax/sytax_review.js
+++ 06_js_syntax/sytax_review.js
@@ -18,3 +18,32 @@ console.log("maskingEmail() start");
 
 // // console.log("maskingEmail(): ", maskingEmail("asd123456@naver.com"));
 // console.log("maskingEmail() executed");
+
+
+/*
+* 마지막 글자를 * 처리한다. (ex. 홍길동 → 홍길*)
+*/
+function maskingName(strName) {
+  if(strName === undefined || strName === '') {
+      return '';
+  }
+  var pattern = /.$/; // 정규식
+  return strName.replace(pattern, "*");
+}
+/*
+* 뒤에서 부터 3글자를 * 처리한다. (ex. 12가3456 → 12가3***)
+*/
+function maskingCar(strCar) {
+  if (strCar == undefined || strCar === '') {
+      return '';
+  }
+  var pattern = /.{3}$/; // 정규식
+  return strCar.replace(pattern, "***");
+}
+
+// 마스킹 이메일 처리 //
+function masking(email) {
+
+ var len = email.split('@')[0].length ;  // ******@gmail.com
+// var len = email.split('@')[0].length-3; // AB***@gamil.com
+ return email.replace(new RegExp('.(?=.{0,' + len + '}@)', 'g'), '*');
